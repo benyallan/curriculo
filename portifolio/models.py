650132112712cd41026363f7e4110bc6cc52d272
Beny Allan <benyallan@yahoo.com.br>
2022-02-24 13:27:13 -0300

    correção de atrelamento feita em portifólios

--- portifolio/models.py
+++ portifolio/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Portifolio(models.Model):
     link = models.URLField(("link"), max_length=200)
     description = models.TextField(("descrição"))
+    person = models.ForeignKey(
+            "core.DadosPessoais", verbose_name=("pessoa"), on_delete=models.CASCADE
+        )
     
     class Meta:
         verbose_name = 'portifolio'
