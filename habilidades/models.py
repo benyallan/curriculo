78ecdb42f913cc017f47eaea387e98196b334f36
Beny Allan <benyallan@yahoo.com.br>
2022-02-25 15:03:33 -0300

    range de idiomas e habilidades de 0-10

--- habilidades/models.py
+++ habilidades/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 
 class Habilidade(models.Model):
 
     name = models.CharField(("nome"), max_length=150)
-    level = models.PositiveSmallIntegerField(("nível"))
+    level = models.PositiveSmallIntegerField(
+            ("nível"), validators=[MaxValueValidator(10)]
+        )
     person = models.ForeignKey(
             "core.dadospessoais", verbose_name=("pessoa"), on_delete=models.CASCADE
         )
