6c1c439fc48e6451c915def2300b12dd69ff9ee9
Beny Allan <benyallan@yahoo.com.br>
2022-03-01 13:00:49 -0300

    inserido redes sociais

--- curriculo/views.py
+++ curriculo/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from core.models import DadosPessoais, Telefone
+from core.models import DadosPessoais, Telefone, RedesSociais
 
 
 def welcome(request):
     pessoa = DadosPessoais.objects.first()
     telefones = Telefone.objects.filter(person=pessoa)
-    dados = {'pessoa': pessoa, 'telefones': telefones}
+    redessociais = RedesSociais.objects.filter(person=pessoa)
+    
+    dados = {
+                'pessoa': pessoa, 'telefones': telefones, 
+                'redessociais': redessociais
+            }
     return render(request, 'index.html', dados)
